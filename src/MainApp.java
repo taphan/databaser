a808bf3b9870ab639b74839bbb0d0d2744b659ea
taphan <trucphan307@gmail.com>
2017-03-08 11:34:22 +0100

    Lagt til eksempelkodene fra BB

---
+++ src/MainApp.java
@@ -0,0 +1,32 @@
+/**
+ * Created by taphan on 08.03.2017.
+ */
+public class MainApp {
+
+    public static void main(String[] args) {
+        // Spør brukeren om å velge et tall og kall deretter chooseActivity(input fra bruker)
+    }
+
+    public void chooseActivity(Integer choice) {
+        /* choice=1: kaller addToDatabase
+        choice=2: kaller viewFromDatabase*/
+    }
+
+    /**
+     * Add data to database
+     */
+    public void addToDatabase() {
+
+    }
+
+    /**
+     * View data from database
+     * User choose between integers:
+     * 1: Uthenting av rapport av den beste treningen den siste uka for hver type av trening. Hva som er kriteriet for «best» må dere bestemme selv.
+     * 2: Uthenting av statistikk fra den siste måneden (antall økter, antall timer og annet)
+     * Kom deretter tilbake til Main
+     */
+    public void viewFromDatabase() {
+
+    }
+}
