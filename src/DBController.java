79d825e0b4338dd905d09d01569e2c881922d7e5
taphan <trucphan307@gmail.com>
2017-03-09 14:49:42 +0100

    Lagt inn chooseActivity

--- src/DBController.java
+++ src/DBController.java
@@ -1,21 +1,18 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
 
-/**
- * Created by taphan on 09.03.2017.
- */
 public class DBController {
     private static final String url = "jdbc:mysql://localhost/treningsdagbok";
 
     private static final String user = "admin";
 
     private static final String password = "database";
+    protected Connection con;
 
     public void connect() {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection(url, user, password);
+            con = DriverManager.getConnection(url, user, password);
             System.out.println("Success");
 
         } catch (Exception e) {
--- src/MainApp.java
+++ src/MainApp.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by taphan on 08.03.2017.
  */
@@ -9,33 +11,48 @@ public class MainApp {
         MainApp main = new MainApp();
         DBController controller = new DBController();
         controller.connect();
-
+        main.chooseActivity();
     }
 
-    public void chooseActivity(Integer choice) {
-        /* 1: kaller addToDatabase
-        2: kaller viewRapport
-        3: kaller viewStatistics*/
+    private void chooseActivity() {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            System.out.println("Velg mellom alternativene ved å taste det tilhørende tallet og trykk Enter:");
+            System.out.println("1: Legg trening til database");
+            System.out.println("2: Viser statistikk de beste treningsøktene den siste måneden.");
+            System.out.println("3: Viser statistikk fra den siste måneden (antall økter, antall timer og annet)");
+            int valg = sc.nextInt();
+            if(valg == 1) {
+                addToDatabase();
+            } else if(valg == 2) {
+                viewRapport();
+            } else if(valg == 3) {
+                viewStatistics();
+            } else {
+                System.out.println("Tast inn et tall mellom 1-3.");
+            }
+        }
+        sc.close();
     }
 
     /**
      * Add data to database
      */
-    public void addToDatabase() {
+    private void addToDatabase() {
 
     }
 
     /**
      * Uthenting av rapport av den beste treningen den siste uka for hver type av trening. Hva som er kriteriet for «best» må dere bestemme selv.
      */
-    public void viewRapport() {
+    private void viewRapport() {
 
     }
 
     /**
      * Uthenting av statistikk fra den siste måneden (antall økter, antall timer og annet)
      */
-    public void viewStatistics() {
+    private void viewStatistics() {
 
     }
 }
