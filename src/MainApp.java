2ae907654fd57f2180f50a7a8ad6826877c6e307
taphan <trucphan307@gmail.com>
2017-03-09 09:09:31 +0100

    Oppdatert mal

--- src/MainApp.java
+++ src/MainApp.java
@@ -5,11 +5,15 @@ public class MainApp {
 
     public static void main(String[] args) {
         // Spør brukeren om å velge et tall og kall deretter chooseActivity(input fra bruker)
+        // while scanner.next.....
+        MainApp main = new MainApp();
+        
     }
 
     public void chooseActivity(Integer choice) {
-        /* choice=1: kaller addToDatabase
-        choice=2: kaller viewFromDatabase*/
+        /* 1: kaller addToDatabase
+        2: kaller viewRapport
+        3: kaller viewStatistics*/
     }
 
     /**
@@ -20,13 +24,16 @@ public class MainApp {
     }
 
     /**
-     * View data from database
-     * User choose between integers:
-     * 1: Uthenting av rapport av den beste treningen den siste uka for hver type av trening. Hva som er kriteriet for «best» må dere bestemme selv.
-     * 2: Uthenting av statistikk fra den siste måneden (antall økter, antall timer og annet)
-     * Kom deretter tilbake til Main
+     * Uthenting av rapport av den beste treningen den siste uka for hver type av trening. Hva som er kriteriet for «best» må dere bestemme selv.
      */
-    public void viewFromDatabase() {
+    public void viewRapport() {
+
+    }
+
+    /**
+     * Uthenting av statistikk fra den siste måneden (antall økter, antall timer og annet)
+     */
+    public void viewStatistics() {
 
     }
 }
