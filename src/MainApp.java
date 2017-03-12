991a197bf994e0a82d3af5b4eda37958c7ca3b00
qaphan3007 <qaphan3007@gmail.com>
2017-03-12 18:06:48 +0100

    viewStatistics()

--- src/MainApp.java
+++ src/MainApp.java
@@ -35,7 +35,30 @@ public class MainApp {
     /**
      * Uthenting av statistikk fra den siste måneden (antall økter, antall timer og annet)
      */
-    public void viewStatistics() {
-
+    public void viewStatistics(Connection connection) {
+        try{
+            Statement statement = conn.createStatement();
+            String query = "SELECT Trening.Dato,Trening.Navn,Trening.Varighet, " +
+                    "count(UtførØvelse.Øvelsesnavn) as Antall_øvelser " +
+                    "FROM UtførØvelse JOIN Trening on UtførØvelse.Treningsnr = Trening.Treningsnr " +
+                    "WHERE Trening.Dato > (NOW() - INTERVAL 1 MONTH) " +
+                    "JOIN Øvelse on UtførØvelse.Øvelsesnavn = Øvelse.Øvelsesnavn " +
+                    "GROUP BY UtførØvelse.Treningsnr " +
+                    "ORDER BY Trening.Dato;";
+            ResultSet resultSet = statement.executeQuery(query);
+            System.out.println("Dette er statistikk av treningene den siste måneden:");
+            if (resultSet.next()) {   // Hvis det er flere rader i tabellen
+                // Format the print message.
+                dato =  rs.getString("Dato");  // getString(column) gets the result in column Dato
+                navn = rs.getString("Navn");
+                varighet =  rs.getString("Varighet");
+                antall_ovelser =  rs.getString("Antall Øvelser");
+                System.out.println("[ Dato: " + dato + ", treningsnavn: " + navn + ", varighet: "
+                                  + varighet + ", antall øvelser: " + antall_ovelser + " ]");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("DB Error when trying to show last month's statistics: "+ e);
+        }
     }
 }
